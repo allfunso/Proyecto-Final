e7ebe14b7490e6953bc753fdcb1d016d203e3d79
allfunso <74326155+allfunso@users.noreply.github.com>
2020-11-14 11:31:53 -0600

    Update App.java
    Llama a la clase que contiene los retos

--- App.java
+++ App.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("There is a target 3 meters high and 20 meters away");
-        System.out.println("You can MOVE, AIM, SHOOT, PICK, HOLD, RELEASE, ELEVATE, PUSH");
-        String selectedOption = scan.next();
-        System.out.println("You selected " + selectedOption);
+        Scanner mainScan = new Scanner(System.in);
+        Robot robot = new Robot();
+        Retos retos = new Retos();
         
-        scan.close();
+        retos.shootingChallenge();
+        retos.defendingChallenge();
+        retos.clawChallenge();
+        
+        mainScan.close();
+        retos.scan.close();
     }
 }
