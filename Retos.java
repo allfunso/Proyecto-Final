8c4882b33ab076684a13ac7815913d1fdc5a0dd6
allfunso <74326155+allfunso@users.noreply.github.com>
2020-11-14 10:25:55 -0600

    Cambie el nombre y unas partes
    Acorté el número de línes porque había cosas repetidas. @CoinDust-png

--- Retos.java
+++ Retos.java
@@ -1,36 +1,30 @@
 import java.util.Scanner;
-public class Proyecto {
+
+public class Retos {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
 
         int force;
-        int numofballs=4;
-        System.out.println(" enter your force");
-        force=scan.nextInt();
+        System.out.println("Enter your force");
 
-        for(int i=0;numofballs>i;numofballs--){
+        for(int numOfBalls=4; numOfBalls>0; numOfBalls--){
+            force = scan.nextInt();
             if(force>=60 && force<70){
                 System.out.println("Too high! you failed");
-                numofballs-=1;
             }
             if(force>50 && force<60){
                 System.out.println("you hit it! just above the center");
-                numofballs-=1;
             }
             if(force==50){
                 System.out.println("You nailed it!");
-                numofballs-=1;
             }
             if(force>=40 && force<50){
                 System.out.println("You hit it! just below the center");
-                numofballs-=1;
             }
             if(force>=30 && force<40){
                System.out.println("Very low! you failed");
-               numofballs-=1;
             }
             System.out.println("Try again");
-            force=scan.nextInt();
         }
         System.out.println("You get ran out fo balls");
         scan.close();
